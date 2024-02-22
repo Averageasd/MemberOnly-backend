f3834593e21bc1a43ec4bb372c7888cf2146748e
Nguyen Huynh <nhuynh9497@gmail.com>
2024-02-22 16:52:48 -0500

    updated cors option

--- api/index.js
+++ api/index.js
@@ -32,7 +32,7 @@ index.use(session({
     resave: false,
     saveUninitialized: true,
     cookie: {
-        secure: false,
+        secure: true,
         maxAge: 1000 * 60 * 60 * 24 // Equals 1 day
     }
 }));
