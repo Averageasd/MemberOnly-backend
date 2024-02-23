3888a794ac040062e3ed7a03a6c18b93a27a8032
Nguyen Huynh <nhuynh9497@gmail.com>
2024-02-22 21:21:36 -0500

    changed origin in cors

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const session = require('express-session');
 const index = express();
 
 index.use(cors({
-    origin: "https://memberonly-frontend-production.up.railway.app/",
+    origin: true,
     methods: ['POST', 'GET'],
     credentials: true,
 }));
